7ce10866f79bb235b6977d58e14bfeb9ed2c09b8
omikronik <yasirceltik9@hotmail.com>
2022-09-30 00:43:27 +0100

    Finished ring spawn in CenterPoint.cs, Added free asset

--- GE1 Examples 2022/Assets/Scripts/CenterPoint.cs
+++ GE1 Examples 2022/Assets/Scripts/CenterPoint.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CenterPoint : MonoBehaviour
 {
-    public GameObject
+    public GameObject rotatingCube;
+    public float CenterOffset;
+    public int RingCount;
 
-    public void GenerateCubes(int num) {
-        for (int i = 0; i > num; i++) {
-            var rad = 2 * MathF.Pi / num * i;
-            var v = MathF.Sin(rad);
-            var h = MathF.Cos(rad);
+    public void GenerateCubes(int numOfObjects, float centerOffset)
+    {
+        for (int i = 0; i < numOfObjects; i++)
+        {
+            var rad = i * 2 * MathF.PI / numOfObjects;
+            var x = MathF.Sin(rad) * centerOffset;
+            var z = MathF.Cos(rad) * centerOffset;
 
-            var spawnDir = new Vector3 (h, 0, v);
+            Vector3 spawnPos = transform.position + new Vector3(x, 0, z);
 
-            var cube = Instantiate ()
+            float angle = -rad*Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
+            Instantiate(rotatingCube, spawnPos, rotation);
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 1; i < RingCount+1; i++)
+        GenerateCubes(6 * i, CenterOffset * i);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.5f, 0);
     }
 }
