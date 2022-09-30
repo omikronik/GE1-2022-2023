bebfde3c050f0c6d75a79b47811b7a74a9982b72
omikronik <yasirceltik9@hotmail.com>
2022-09-30 17:03:40 +0100

    added color shifting

--- GE1 Examples 2022/Assets/Scripts/CenterPoint.cs
+++ GE1 Examples 2022/Assets/Scripts/CenterPoint.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+using Random = UnityEngine.Random;
+
 public class CenterPoint : MonoBehaviour
 {
     public GameObject rotatingCube;
     public float CenterOffset;
     public int RingCount;
+
+    public List<GameObject> Amoguses = new List<GameObject>();
 
     public void GenerateCubes(int numOfObjects, float centerOffset)
     {
@@ -21,15 +25,29 @@ public class CenterPoint : MonoBehaviour
 
             float angle = -rad*Mathf.Rad2Deg;
             Quaternion rotation = Quaternion.Euler(-90, angle, 0);
-            Instantiate(rotatingCube, spawnPos, rotation);
+            GameObject go = Instantiate(rotatingCube, spawnPos, rotation);
+            go.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1f, 1f);
+            Amoguses.Add(go);
+        }
+    }
+
+    public void RandomizeColor() {
+        var randColor = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1f, 1f);
+
+        foreach (var Amogus in Amoguses)
+        {
+            Amogus.GetComponent<Renderer>().material.color = randColor;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i < RingCount+1; i++)
-        GenerateCubes(6 * i, CenterOffset * i);
+        for (int i = 1; i < RingCount+1; i++) {
+            GenerateCubes(6 * i, CenterOffset * i);
+        }
+        
+        InvokeRepeating("RandomizeColor", 0.5f, 1.0f);
     }
 
     // Update is called once per frame
--- GE1 Examples 2022/Assets/Scripts/MyTank.cs
+++ GE1 Examples 2022/Assets/Scripts/MyTank.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tank : MonoBehaviour
+public class MyTank : MonoBehaviour
 {
     public float speed = 5f;
     // Start is called before the first frame update
